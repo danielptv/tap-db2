3c66013a5f09d322d19de292d63e0115f732a02d
Daniel Purtov <daniel.purtov@gmail.com>
2023-09-20 10:06:33 +0200

    fix: workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,39 @@
 """Test Configuration."""
+import datetime
+
+import sqlalchemy
+from faker import Faker
+from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table, text
 
 pytest_plugins = ("singer_sdk.testing.pytest_plugin",)
+
+TEST_SQLALCHEMY_URL = "ibm_db_sa://db2inst1:password@localhost:50000/testdb;"
+
+TABLE_NAME = "TEST_TABLE"
+
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
+    engine = sqlalchemy.create_engine(TEST_SQLALCHEMY_URL)
+    fake = Faker()
+
+    date1 = datetime.date(2022, 11, 1)
+    date2 = datetime.date(2022, 11, 30)
+    metadata_obj = MetaData()
+    test_table = Table(
+        TABLE_NAME,
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("updated_at", DateTime(), nullable=False),
+        Column("name", String(30)),
+    )
+    with engine.connect() as conn:
+        conn.execute(text(f"DROP TABLE IF EXISTS {TABLE_NAME}"))
+        metadata_obj.create_all(conn)
+        for _ in range(1000):
+            insert = test_table.insert().values(updated_at=fake.date_between(date1, date2), name=fake.name())
+            conn.execute(insert)
+        conn.commit()
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,11 +2,7 @@
 
 import datetime
 
-import pytest
-import sqlalchemy
-from faker import Faker
 from singer_sdk.testing import SuiteConfig, get_tap_test_class
-from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table, text
 
 from tap_db2.tap import TapDB2
 
@@ -22,44 +18,5 @@ TEST_CONFIG = {
 # TODO: No records found for stream 'DB2INST1-TEST_TABLE'
 TEST_SUITE_CONFIG = SuiteConfig(ignore_no_records_for_streams=["DB2INST1-TEST_TABLE"])
 
-TEST_SQLALCHEMY_URL = "ibm_db_sa://db2inst1:password@localhost:50000/testdb;"
-
-TABLE_NAME = "TEST_TABLE"
-
-
-def setup_test_table(table_name, sqlalchemy_url):
-    """setup any state specific to the execution of the given module."""
-    engine = sqlalchemy.create_engine(sqlalchemy_url)
-    fake = Faker()
-
-    date1 = datetime.date(2022, 11, 1)
-    date2 = datetime.date(2022, 11, 30)
-    metadata_obj = MetaData()
-    test_table = Table(
-        table_name,
-        metadata_obj,
-        Column("id", Integer, primary_key=True),
-        Column("updated_at", DateTime(), nullable=False),
-        Column("name", String(30)),
-    )
-    with engine.connect() as conn:
-        conn.execute(text(f"DROP TABLE IF EXISTS {table_name}"))
-        metadata_obj.create_all(conn)
-        for _ in range(1000):
-            insert = test_table.insert().values(updated_at=fake.date_between(date1, date2), name=fake.name())
-            conn.execute(insert)
-        conn.commit()
-
-
 # Run standard built-in tap tests from the SDK:
-TapDB2Test = get_tap_test_class(tap_class=TapDB2, config=TEST_CONFIG, suite_config=TEST_SUITE_CONFIG)
-
-
-class TestTapDB2(TapDB2Test):
-    table_name = TABLE_NAME
-    sqlalchemy_url = TEST_SQLALCHEMY_URL
-
-    @pytest.fixture(scope="class")
-    def resource(self):
-        setup_test_table(self.table_name, self.sqlalchemy_url)
-        # yield
+TestTapDB2 = get_tap_test_class(tap_class=TapDB2, config=TEST_CONFIG, suite_config=TEST_SUITE_CONFIG)
