1336b3e23e1eb83dc7a3224e94e658ea874eafb3
Daniel Purtov <daniel.purtov@gmail.com>
2023-10-04 14:49:03 +0200

    test: Enable all built-in tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,21 +2,20 @@
 
 import datetime
 
-from singer_sdk.testing import SuiteConfig, get_tap_test_class
+from singer_sdk.testing import get_tap_test_class
 
 from tap_db2.tap import TapDB2
 
 TEST_CONFIG = {
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "host": "0.0.0.0",
+    "host": "localhost",
     "port": "50000",
     "database": "testdb",
     "user": "db2inst1",
     "password": "password",
 }
 
-# TODO: No records found for stream 'DB2INST1-TEST_TABLE'
-TEST_SUITE_CONFIG = SuiteConfig(ignore_no_records_for_streams=["DB2INST1-TEST_TABLE"])
 
-# Run standard built-in tap tests from the SDK:
-TestTapDB2 = get_tap_test_class(tap_class=TapDB2, config=TEST_CONFIG, suite_config=TEST_SUITE_CONFIG)
+TestTapDB2 = get_tap_test_class(
+    tap_class=TapDB2, config=TEST_CONFIG, catalog="tests/data/catalog.json", include_stream_tests=True
+)
