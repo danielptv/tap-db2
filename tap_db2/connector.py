d64aed02aead6515da6a347c3700b385b2cdc9a0
Daniel <daniel.purtov@gmail.com>
2024-02-16 10:40:33 +0100

    fix: ignore_views not working

--- tap_db2/connector.py
+++ tap_db2/connector.py
@@ -90,10 +90,10 @@ class DB2Connector(SQLConnector):
                 inspected,
                 schema_name,
             ):
-                if (
-                    not re.match(SUPPLIED_USER_TABLES_PATTERN, table_name, re.IGNORECASE)
-                    or (is_view and "ignore_views" in self.config and self.config["ignore_views"])
-                    or ("ignore_supplied_tables" in self.config and not self.config["ignore_supplied_tables"])
+                if is_view and "ignore_views" in self.config and self.config["ignore_views"]:
+                    continue
+                if not re.match(SUPPLIED_USER_TABLES_PATTERN, table_name, re.IGNORECASE) or (
+                    "ignore_supplied_tables" in self.config and not self.config["ignore_supplied_tables"]
                 ):
                     # Filter by schema
                     # Connection parameter 'CURRENTSCHEMA=mySchema;' doesn't work
